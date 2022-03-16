929e1b70d9a320c6be65541af6743f40503e443f
Ariel Andrielli Rodrigues da Silva <arielandrielli100@gmail.com>
2022-03-16 16:43:05 -0300

    Operacoes assincronas usando Task(async, await)

--- Sale Web Mvc/Sale Web Mvc/Services/DepartmentService.cs
+++ Sale Web Mvc/Sale Web Mvc/Services/DepartmentService.cs
@@ -2,6 +2,8 @@
 using Sale_Web_Mvc.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Sale_Web_Mvc.Services
 {
@@ -14,9 +16,9 @@ namespace Sale_Web_Mvc.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>>  FindAllAsync() //Para você transformar a operação em assíncrona você deve colocar o Task na frente do 'List<Department>'. O sulfixo 'Async' é uma recomendação do C#
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync(); //Pra que se possa fazer uma chamada assíncrona no método é necessário avisar ao complilador que isso é uma chamada assíncrona 
         }
     }
 }
